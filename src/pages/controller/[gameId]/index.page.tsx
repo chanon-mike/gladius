20c4b498e4e1caddf466a2bd139e07996f91ecea
enbipaipu <s1f102300122@iniad.org>
2023-08-02 22:05:35 +0900

    簡易的ですが

--- src/pages/controller/[gameId]/index.page.tsx
+++ src/pages/controller/[gameId]/index.page.tsx
@@ -6,23 +6,30 @@ const ControllerPage = () => {
   const { gameId } = router.query;
   console.log(router.query);
 
+  const test = () => {
+    console.log('aaaaaaaaaaaaa');
+  };
+
   return (
     <>
       <div>
         <h1>Controller Page</h1>
         <p>User ID: {gameId}</p>
       </div>
-      <div className={styles.controller}>
-        <div className={styles.cross}>
-          <div className={styles.top} />
-          <div className={styles.left} />
-          <div className={styles.right} />
-          <div className={styles.bottom} />
-          <div className={styles.center} />
-        </div>
-        <div className={styles.button}>
-          <div className={styles.btnone} />
-          <div className={styles.btntwo} />
+      <div className={styles.under}>
+        <div className={styles.controller}>
+          <div className={styles.cross}>
+            <div className={styles.top} />
+            <div className={styles.left} />
+            <div className={styles.right} />
+            <div className={styles.bottom} />
+            <div className={styles.center} />
+          </div>
+          <div className={styles.button}>
+            <div className={styles.btnone} onClick={test}>
+              <div className={styles.test} />
+            </div>
+          </div>
         </div>
       </div>
     </>
